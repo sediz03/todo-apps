78d370218ac662c81858dc76bb620e833eb46a52
jackpot <sediz03@gmail.com>
2023-02-19 15:02:01 +0900

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,153 @@
 import React, { useEffect, useState } from "react";
+import uuid from "react-uuid";
 
 function App() {
-  const [number, setNumber] = useState(0); // 결과값
-  const [tempNumber, setTempNumber] = useState(1); //input값
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: uuid(),
+      title: "1st title",
+      contents: "1st contents",
+      isDone: false,
+    },
+    {
+      id: uuid(),
+      title: "2nd title",
+      contents: "2nd contents",
+      isDone: false,
+    },
+    {
+      id: uuid(),
+      title: "3rd title",
+      contents: "3rd contents",
+      isDone: false,
+    },
+  ]);
 
-  const onPlusButtonClickHandler = () => {
-    setNumber(number + +tempNumber);
+  const onDeleteButtonClickHandler = (id) => {
+    setTodos(
+      todos.filter((filterItem) => {
+        return filterItem.id !== id;
+      })
+    );
   };
 
-  const onMinusButtonClickHandler = () => {
-    setNumber(number - +tempNumber);
-  };
+  useEffect(() => {
+    console.log("title", title);
+    console.log("contents", contents);
+  }, [title, contents]);
 
   return (
-    <div>
-      {number}
-      <br />
-      <button onClick={onPlusButtonClickHandler}>+</button>
-      <button onClick={onMinusButtonClickHandler}>-</button>
-      <br />
-      <input
-        type="number"
-        value={tempNumber}
-        onChange={function (event) {
-          setTempNumber(event.target.value);
-        }}
-      />
-      <br />
-    </div>
+    <>
+      <div>
+        <br />
+        title :
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        ></input>
+        <br />
+        contents :
+        <input
+          type="text"
+          value={contents}
+          onChange={(e) => {
+            setContents(e.target.value);
+          }}
+        ></input>
+        <br />
+        <button
+          onClick={() => {
+            setTodos([
+              ...todos,
+              {
+                id: uuid(),
+                title,
+                contents,
+                idDone: false,
+              },
+            ]);
+          }}
+        >
+          save
+        </button>
+      </div>
+      <hr></hr>
+      <div>
+        <h5>todos</h5>
+        <br></br>
+        {todos
+          .filter((filterItem) => {
+            return !filterItem.isDone;
+          })
+          .map((mapItem) => {
+            return (
+              <div key={mapItem.id}>
+                <span>title : {mapItem.title}</span>
+                <br />
+                <span>contents : {mapItem.contents}</span>
+                <br></br>
+                <button
+                  onClick={() => {
+                    const newTodos = todos.map((doneItem) => {
+                      if (doneItem.id === mapItem.id) {
+                        return { ...doneItem, isDone: true };
+                      } else {
+                        return doneItem;
+                      }
+                    });
+                    setTodos(newTodos);
+                  }}
+                >
+                  done
+                </button>
+                <button onClick={() => onDeleteButtonClickHandler(mapItem.id)}>
+                  delete
+                </button>
+                <p></p>
+              </div>
+            );
+          })}
+        <hr></hr>
+        <h5>done</h5>
+        {todos
+          .filter((filterItem) => {
+            return filterItem.isDone;
+          })
+          .map((mapItem) => {
+            return (
+              <div key={mapItem.id}>
+                <span>title : {mapItem.title}</span>
+                <br />
+                <span>contents : {mapItem.contents}</span>
+                <br></br>
+                <button
+                  onClick={() => {
+                    const newTodos = todos.map((doneItem) => {
+                      if (doneItem.id === mapItem.id) {
+                        return { ...doneItem, isDone: false };
+                      } else {
+                        return doneItem;
+                      }
+                    });
+                    setTodos(newTodos);
+                  }}
+                >
+                  cancle
+                </button>
+                <button onClick={() => onDeleteButtonClickHandler(mapItem.id)}>
+                  delete
+                </button>
+                <p></p>
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/DoneList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DoneList = () => {
+  return <div>DoneList</div>;
+};
+
+export default DoneList;
